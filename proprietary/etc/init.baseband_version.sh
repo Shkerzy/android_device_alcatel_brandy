a1a8d623a7a552f4a834b7e93ac97ac265283925
Shkerzy <Shkerzy@GitHub>
2013-01-13 15:57:29 +0800

    Update baseband version's script
    This is a full version of baseband. Received from gsm modem.
    ATI1 - Manufacturer: Alcatel
           Model: one_touch_990
           Revision: M76XX-TSNCJOLYM-6180  1  [Jun 20 2011 03:00:00]

--- proprietary/etc/init.baseband_version.sh
+++ proprietary/etc/init.baseband_version.sh
@@ -1,4 +1,4 @@
 #!/system/bin/sh
 # Script for define Baseband Version Alcatel OT-990
 
-apversion && sleep 5s && setprop gsm.version.baseband 6180;
+apversion && sleep 5s && setprop gsm.version.baseband M76XX-TSNCJOLYM-6180;
