32412b838100164c7a5b5b20d687201642c44dd2
Shkerzy <niksamnoy@gmail.com>
2013-01-19 17:11:55 +0800

    remove menu visibility toggle

--- recovery/recovery_ui.c
+++ recovery/recovery_ui.c
@@ -37,18 +37,6 @@ int device_recovery_start() {
     return 0;
 }
 
-int device_toggle_display(volatile char* key_pressed, int key_code) {
-    int alt = key_pressed[KEY_LEFTALT] || key_pressed[KEY_RIGHTALT];
-    if (alt && key_code == KEY_L)
-        return 1;
-    // allow toggling of the display if the correct key is pressed, and the display toggle is allowed or the display is currently off
-    if (ui_get_showing_back_button()) {
-        return 0;
-        //return get_allow_toggle_display() && (key_code == KEY_HOME || key_code == KEY_MENU || key_code == KEY_END);
-    }
-    return get_allow_toggle_display() && (key_code == KEY_HOME || key_code == KEY_MENU || key_code == KEY_POWER || key_code == KEY_END);
-}
-
 int device_reboot_now(volatile char* key_pressed, int key_code) {
     return 0;
 }
@@ -62,21 +50,12 @@ int device_handle_key(int key_code, int visible) {
             case KEY_VOLUMEDOWN:
                 return HIGHLIGHT_UP;
 
-            case KEY_POWER:
-                if (ui_get_showing_back_button()) {
-                    return SELECT_ITEM;
-                }
-                if (!get_allow_toggle_display())
-                    return GO_BACK;
-                break;
-
             case KEY_MENU:
             case KEY_HOME:
                 return SELECT_ITEM;
                 
             case KEY_BACK:
-                if (!get_allow_toggle_display())
-                    return GO_BACK;
+                return GO_BACK;
         }
     }
 
